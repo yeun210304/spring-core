312e0c9980af976f8dba4ba2393cc9c4ad89e68b
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-02-25 20:26:18 +0900

    컴포넌트 스캔 위치 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // @Component가 붙은 클래스를 찾아서 스프링 빈으로 등록해준다.
 @ComponentScan(
-    // 제외항목
+    basePackages =  "hello.core", // 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다.
     excludeFilters = @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = Configuration.class)
 ) 
 public class AutoAppConfig {
